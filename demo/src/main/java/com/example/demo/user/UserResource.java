11d0fbb1fc4185f7bd29a7c1817936c9f27f08bd
jirawan-chuapradit <kazuomimeaw@gmail.com>
2019-07-01 16:43:00 +0700

    enchancing POST method to return correct http status code
    201 created

--- demo/src/main/java/com/example/demo/user/UserResource.java
+++ demo/src/main/java/com/example/demo/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.example.demo.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -30,8 +33,17 @@ public class UserResource {
 	// input - details of user
 	// output - CREATED & Return the created URI
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User saveUser = service.save(user);
+		
+		//created
+		// /users/{id}
+		URI location = ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(saveUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 	
 }
