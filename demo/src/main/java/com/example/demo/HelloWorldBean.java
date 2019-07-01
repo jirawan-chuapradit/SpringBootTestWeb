ed6dbc117db0e2326a04bfb7341d7238b9f152f1
jirawan-chuapradit <kazuomimeaw@gmail.com>
2019-07-01 14:35:20 +0700

    intial

---
+++ demo/src/main/java/com/example/demo/HelloWorldBean.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String string) {
+		this.message = string;
+	}
+	
+	
+	
+	public String getMessage() {
+		return message;
+	}
+
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+	
+	
+}
---
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	/**
+	 * GET 
+	 * URI -/hello-world
+	 * method-"Hello World"
+	 * @return Hello World
+	 */
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	/**
+	 * Hello World Bean
+	 * @return 
+	 * @return
+	 */
+	@GetMapping(path = "/hello-world-bean")
+	public  HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world-bean/path-variable/{name}")
+	public  HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	
+}
