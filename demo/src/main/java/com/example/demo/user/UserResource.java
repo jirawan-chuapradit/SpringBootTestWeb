6e44b248ee470cfaa630d4935d17cbc677094b2d
jirawan-chuapradit <kazuomimeaw@gmail.com>
2019-07-01 16:29:30 +0700

    implement POST method to create User Resource

--- demo/src/main/java/com/example/demo/user/UserResource.java
+++ demo/src/main/java/com/example/demo/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +25,13 @@ public class UserResource {
 	@GetMapping("/user/{id}")
 	public User getUser(@PathVariable int id) {
 		return service.findOne(id);
+	}
+	
+	// input - details of user
+	// output - CREATED & Return the created URI
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		User saveUser = service.save(user);
 	}
 	
 }
