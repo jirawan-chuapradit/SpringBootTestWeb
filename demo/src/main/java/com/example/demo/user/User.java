20c460d9e170521c1c7377c9a21ae8f24ab575cf
jirawan-chuapradit <kazuomimeaw@gmail.com>
2019-07-04 11:10:28 +0700

    actuator

--- demo/src/main/java/com/example/demo/user/User.java
+++ demo/src/main/java/com/example/demo/user/User.java
@@ -2,18 +2,30 @@ package com.example.demo.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import org.springframework.data.annotation.Id;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+
+@ApiModel(description="All details about the user.")
 public class User {
 
-	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message = "Name should have alteast 2 character")
+	@ApiModelProperty(notes="Name should have atleast 2 characters")
 	private String name;
 	
 	@Past()
+	@ApiModelProperty(notes="Birrh date should be in the past")
 	private Date birthDate;
 	
 	
